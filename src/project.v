d5990fc33bcf381277ce30b04ac3ea4ab7a560cb
Jorge Angarita Pérez <68038923+Gior-gio@users.noreply.github.com>
2025-11-10 11:29:51 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Onchip_RCOx2 (
+module tt_um_Onchip_VCOx2 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
